1e310a30d78333ac0da510f0954a4fbe43fd97f7
krshmt <tourekris16@gmail.com>
2025-02-27 09:50:08 +0100

    animation entrée et sorti du composant gallery

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Gallery from "./components/Gallery";
 import Description from "./components/Description";
-import { LayoutGroup } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
   return (
       <Router>
-        <LayoutGroup>
+        <AnimatePresence>
             <Routes>
               <Route path="/" element={<Gallery />} />
               <Route path="/description" element={<Description />} />
             </Routes>
-        </LayoutGroup>
+        </AnimatePresence>
       </Router>
   );
 }
--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -5,110 +5,132 @@ import { motion } from "framer-motion";
 import "./styles.css";
 
 function Gallery() {
-    const galleryRef = useRef<HTMLDivElement>(null);
-    const [items, setItems] = useState<any[]>([]);
-    const [isImageClicked, setIsImageClicked] = useState(false);
+  const galleryRef = useRef<HTMLDivElement>(null);
+  const [items, setItems] = useState<any[]>([]);
+  const [isImageClicked, setIsImageClicked] = useState(false);
 
-    useEffect(() => {
-        const generateItems = () => {
-            const rows = [
-                { id: 1, count: 4 },
-                { id: 2, count: 3 },
-                { id: 3, count: 4 },
-            ];
-            const newItems = rows.flatMap((row) => {
-                return Array.from({ length: row.count }, (_, index) => {
-                    const itemId = `${row.id}-${index + 1}`;
-                    const image = images.find((i) => i.id === itemId);
-                    return {
-                        id: itemId,
-                        rowId: row.id,
-                        image: image || "",
-                    };
-                });
-            });
-            setItems(newItems);
-        };
-        generateItems();
-    }, []);
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      if (isImageClicked) return;
+      const { clientX, clientY, currentTarget } = e;
+      const { width, height } = (
+        currentTarget as HTMLElement
+      ).getBoundingClientRect();
+      const centerX = width / 2;
+      const centerY = height / 2;
 
-    useEffect(() => {
-        if (items.length === 0) return;
+      const sensitivity = 2.3;
+      const deltaX = (centerX - clientX) / sensitivity;
+      const deltaY = (centerY - clientY) / sensitivity;
 
-        const itemsElements = document.querySelectorAll(".item");
-        const positions = [
-            { top: "25%", left: "10%" },
-            { top: "20%", left: "25%" },
-            { top: "40%", left: "40%" },
-            { top: "40%", left: "80%" },
-            { top: "65%", left: "25%" },
-            { top: "0%", left: "40%" },
-            { top: "30%", left: "55%" },
-            { top: "60%", left: "60%" },
-            { top: "10%", left: "70%" },
-            { top: "0%", left: "100%" },
-            { top: "20%", left: "30%" },
-        ];
+      if (galleryRef.current) {
+        galleryRef.current.style.transform = `translate(calc(-50% + ${deltaX}px), calc(-50% + ${deltaY}px))`;
+      }
+    };
 
-        itemsElements.forEach((item, index) => {
-            const pos = positions[index % positions.length];
-            item.style.position = "absolute";
-            item.style.top = pos.top;
-            item.style.left = pos.left;
+    const container = document.querySelector(".container");
+    container?.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      container?.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [isImageClicked]);
+
+  useEffect(() => {
+    const generateItems = () => {
+      const rows = [
+        { id: 1, count: 4 },
+        { id: 2, count: 3 },
+        { id: 3, count: 4 },
+      ];
+      const newItems = rows.flatMap((row) => {
+        return Array.from({ length: row.count }, (_, index) => {
+          const itemId = `${row.id}-${index + 1}`;
+          const image = images.find((i) => i.id === itemId);
+          return {
+            id: itemId,
+            rowId: row.id,
+            image: image || "",
+          };
         });
-    }, [items]);
+      });
+      setItems(newItems);
+    };
+    generateItems();
+  }, []);
 
-    useEffect(() => {
-        const handleMouseMove = (e: MouseEvent) => {
-            if (isImageClicked) return;
-            const { clientX, clientY, currentTarget } = e;
-            const { width, height } = (currentTarget as HTMLElement).getBoundingClientRect();
-            const centerX = width / 2;
-            const centerY = height / 2;
+  useEffect(() => {
+    if (items.length === 0) return;
 
-            const sensitivity = 2.3;
-            const deltaX = (centerX - clientX) / sensitivity;
-            const deltaY = (centerY - clientY) / sensitivity;
+    const itemsElements = document.querySelectorAll(".item");
+    const positions = [
+      { top: "25%", left: "10%" },
+      { top: "20%", left: "25%" },
+      { top: "40%", left: "40%" },
+      { top: "40%", left: "80%" },
+      { top: "65%", left: "25%" },
+      { top: "0%", left: "40%" },
+      { top: "30%", left: "55%" },
+      { top: "60%", left: "60%" },
+      { top: "10%", left: "70%" },
+      { top: "0%", left: "100%" },
+      { top: "20%", left: "40%" },
+    ];
 
-            if (galleryRef.current) {
-                galleryRef.current.style.transform = `translate(calc(-50% + ${deltaX}px), calc(-50% + ${deltaY}px))`;
-            }
-        };
+    itemsElements.forEach((item, index) => {
+      const pos = positions[index % positions.length];
+      item.style.position = "absolute";
+      item.style.top = pos.top;
+      item.style.left = pos.left;
+    });
+  }, [items]);
 
-        const container = document.querySelector(".container");
-        container?.addEventListener("mousemove", handleMouseMove);
+  const handleClick = (
+    id: string,
+    event: React.MouseEvent<HTMLAnchorElement>
+  ) => {
+    event.preventDefault();
+    setIsImageClicked(true);
 
-        return () => {
-            container?.removeEventListener("mousemove", handleMouseMove);
-        };
-    }, [isImageClicked]);
+    const items = document.querySelectorAll(".item");
+    items.forEach((item) => item.classList.add("active"));
 
-    return (
-        <div className="container">
-            <div className="gallery__container" ref={galleryRef}>
-                <div className="gallery">
-                {items.map((item) => {
-                    console.log("Gallery layoutId: ", `image-${item.id}`);  // Affiche l'ID dans la console
-                    return (
-                        <Link
-                            key={item.id}
-                            to={`/description?id=${encodeURIComponent(item.id)}`}
-                            className="item"
-                            data-id={item.id}
-                            onClick={() => setIsImageClicked(true)}
-                        >
-                            <motion.img
-                                src={item.image.src}
-                                alt={item.image.title}
-                                className="img"
-                            />
-                        </Link>
-                    );
-                })}
-                </div>
-            </div>
+    setTimeout(() => {
+      window.location.href = `/description?id=${encodeURIComponent(id)}`;
+    }, 900); // Assurez-vous que le timeout est légèrement plus long que l'animation CSS
+  };
+
+  return (
+    <motion.div
+      className="container"
+      initial={{ scale: 0.7, opacity: 0 }}
+      animate={{ scale: 1, opacity: 1 }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
+    >
+      <div className="gallery__container" ref={galleryRef}>
+        <div className="gallery">
+          {items.map((item) => {
+            console.log("Gallery layoutId: ", `image-${item.id}`); // Affiche l'ID dans la console
+            return (
+              <Link
+                key={item.id}
+                to={`/description?id=${encodeURIComponent(item.id)}`}
+                className="item"
+                data-id={item.id}
+                onClick={(e) => handleClick(item.id, e)}
+              >
+                <motion.img
+                  src={item.image.src}
+                  alt={item.image.title}
+                  className="img"
+                />
+              </Link>
+            );
+          })}
         </div>
-    );
+      </div>
+    </motion.div>
+  );
 }
 
 export default Gallery;
