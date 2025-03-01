19b444fd344edbeb603f3fb62808282ac61a12df
krshmt <tourekris16@gmail.com>
2025-03-01 11:26:20 +0100

    Initial commit ABout page

--- src/data/imageDetail.js
+++ src/data/imageDetail.js
@@ -4,7 +4,62 @@ const imageVoirPlus = [
         src_1: 'https://www.fubiz.net/wp-content/uploads/2019/08/01.jpg',
         src_2: 'https://www.fubiz.net/wp-content/uploads/2019/08/04.jpg',
         src_3: 'https://www.fubiz.net/wp-content/uploads/2019/08/05.jpg',
-    }
+    },
+    {
+        id: "1-2",
+        src_1: 'https://www.fubiz.net/wp-content/uploads/2019/08/01.jpg',
+        src_2: 'https://www.fubiz.net/wp-content/uploads/2019/08/04.jpg',
+        src_3: 'https://www.fubiz.net/wp-content/uploads/2019/08/05.jpg',
+    },
+    {
+        id: "1-3",
+        src_1: 'https://www.fubiz.net/wp-content/uploads/2019/08/01.jpg',
+        src_2: 'https://www.fubiz.net/wp-content/uploads/2019/08/04.jpg',
+        src_3: 'https://www.fubiz.net/wp-content/uploads/2019/08/05.jpg',
+    },
+    {
+        id: "1-4",
+        src_1: 'https://www.fubiz.net/wp-content/uploads/2019/08/01.jpg',
+        src_2: 'https://www.fubiz.net/wp-content/uploads/2019/08/04.jpg',
+        src_3: 'https://www.fubiz.net/wp-content/uploads/2019/08/05.jpg',
+    },
+    {
+        id: "2-1",
+        src_1: 'https://www.fubiz.net/wp-content/uploads/2019/08/01.jpg',
+        src_2: 'https://www.fubiz.net/wp-content/uploads/2019/08/04.jpg',
+        src_3: 'https://www.fubiz.net/wp-content/uploads/2019/08/05.jpg',
+    },
+    {
+        id: "2-2",
+        src_1: 'https://www.fubiz.net/wp-content/uploads/2019/08/01.jpg',
+        src_2: 'https://www.fubiz.net/wp-content/uploads/2019/08/04.jpg',
+        src_3: 'https://www.fubiz.net/wp-content/uploads/2019/08/05.jpg',
+    },
+    {
+        id: "2-3",
+        src_1: 'https://www.fubiz.net/wp-content/uploads/2019/08/01.jpg',
+        src_2: 'https://www.fubiz.net/wp-content/uploads/2019/08/04.jpg',
+        src_3: 'https://www.fubiz.net/wp-content/uploads/2019/08/05.jpg',
+    },
+    {
+        id: "3-1",
+        src_1: 'https://www.fubiz.net/wp-content/uploads/2019/08/01.jpg',
+        src_2: 'https://www.fubiz.net/wp-content/uploads/2019/08/04.jpg',
+        src_3: 'https://www.fubiz.net/wp-content/uploads/2019/08/05.jpg',
+    },
+    {
+        id: "3-2",
+        src_1: 'https://www.fubiz.net/wp-content/uploads/2019/08/01.jpg',
+        src_2: 'https://www.fubiz.net/wp-content/uploads/2019/08/04.jpg',
+        src_3: 'https://www.fubiz.net/wp-content/uploads/2019/08/05.jpg',
+    },
+    {
+        id: "3-3",
+        src_1: 'https://www.fubiz.net/wp-content/uploads/2019/08/01.jpg',
+        src_2: 'https://www.fubiz.net/wp-content/uploads/2019/08/04.jpg',
+        src_3: 'https://www.fubiz.net/wp-content/uploads/2019/08/05.jpg',
+    },
+    
 ]
 
 export default imageVoirPlus;
